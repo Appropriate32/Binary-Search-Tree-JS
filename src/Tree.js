95ccd4edd005c882e98c0ec5c2cca17f69405f01
Appropriate32 <frostghost32@gmail.com>
2026-07-01 16:31:17 +0500

    buildTree function properly implemented inside Tree

--- src/Tree.js
+++ src/Tree.js
@@ -2,8 +2,20 @@ import Node from "./Node.js";
 
 class Tree {
   constructor(arr) {
-    this.arr = arr;
-    this.root = null;
+    this.arr = [...new Set(arr)].sort((a, b) => a - b);
+    this.root = this.buildTree(arr, 0, this.arr.length - 1);
+  }
+
+  buildTree(array, start, end) {
+    if (start > end) return null;
+
+    let mid = start + Math.floor((end - start) / 2);
+    let root = new Node(array[mid]);
+
+    root.left = this.buildTree(array, start, mid - 1);
+    root.right = this.buildTree(array, mid + 1, end);
+
+    return root;
   }
 }
 
